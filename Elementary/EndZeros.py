b15503d6b63ef1ba77ae599eda45d2de5c7d16f1
Tirso del Álamo <tirsodelalamomartin@gmail.com>
2020-04-12 22:00:23 +0200

    Adding new files

---
+++ Elementary/EndZeros.py
@@ -0,0 +1,20 @@
+
+def end_zeros(num: int) -> int:
+    num=str(num)
+    
+    return len(num)-len(num.rstrip('0'))
+
+
+
+if __name__ == '__main__':
+    print("Example:")
+    print(end_zeros(0))
+
+    # These "asserts" are used for self-checking and not for an auto-testing
+    assert end_zeros(0) == 1
+    assert end_zeros(1) == 0
+    assert end_zeros(10) == 1
+    assert end_zeros(101) == 0
+    assert end_zeros(245) == 0
+    assert end_zeros(100100) == 2
+    print("Coding complete? Click 'Check' to earn cool rewards!")
---
+++ Home/DaysBetween.py
@@ -0,0 +1,19 @@
+
+import datetime
+def days_diff(a, b):
+    # your code here
+    
+    a = datetime.date(*a)
+    b = datetime.date(*b)
+    return abs(b-a).days
+
+
+if __name__ == '__main__':
+    print("Example:")
+    print(days_diff((1982, 4, 19), (1982, 4, 22)))
+
+    # These "asserts" are used for self-checking and not for an auto-testing
+    assert days_diff((1982, 4, 19), (1982, 4, 22)) == 3
+    assert days_diff((2014, 1, 1), (2014, 8, 27)) == 238
+    assert days_diff((2014, 8, 27), (2014, 1, 1)) == 238
+    print("Coding complete? Click 'Check' to earn cool rewards!")
